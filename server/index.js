9d48aab93ae3b24e9fce477a602d6e7278a54fdb
ibnsina-92 <peshousers@gmail.com>
2026-01-21 01:34:03 +0500

    ㅤ I B N  ·  S I N A ㅤ

--- server/index.js
+++ server/index.js
@@ -1,1 +1,11 @@
+import express from 'express';
 
+const app = express();
+
+app.get('/health', (req, res) => {
+  res.json({ status: 'OK' });
+});
+
+app.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
+});
